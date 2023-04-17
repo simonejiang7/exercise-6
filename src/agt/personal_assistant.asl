982333deada5d2b414744b26fbd90b7e53c96e26
Xuemei Jiang <simonejiang.pku@gmail.com>
2023-04-17 15:07:01 +0200

    task1.1

--- src/agt/personal_assistant.asl
+++ src/agt/personal_assistant.asl
@@ -13,7 +13,19 @@
 */
 @start_plan
 +!start : true <-
-    .print("Hello world").
+    .print("Hello world");
+    !create_and_send.
+
+// the agent has a plan for creating a DweetArtifact, and then using the artifact to send messages.
+@create_artifact_and_send_message_plan
++!create_and_send : true
+  <- !setUpDweetArtifact;
+    sendMessage("New Message from Agent");
+    .print("Message sent").
+
+// create a DweetArtifact
++!setUpDweetArtifact : true
+  <- makeArtifact("dweet_artifact","room.DweetArtifact").
 
 /* Import behavior of agents that work in CArtAgO environments */
 { include("$jacamoJar/templates/common-cartago.asl") }
--- src/env/room/DweetArtifact.java
+++ src/env/room/DweetArtifact.java
@@ -1,11 +1,46 @@
 package room;
 
 import cartago.Artifact;
+import cartago.OPERATION;
+import cartago.ObsProperty;
+
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * A CArtAgO artifact that provides an operation for sending messages to agents 
  * with KQML performatives using the dweet.io API
  */
 public class DweetArtifact extends Artifact {
-    
+
+    void init() {
+        // defineObsProperty("DweetArtifact",0);
+    }
+
+    @OPERATION void sendMessage(String msg) throws JSONException {
+
+        // ObsProperty prop = getObsProperty("newMessage");
+        // prop.updateValue(msg);
+
+        JSONObject jsonBody = new JSONObject();
+        jsonBody.put("newMessage", msg);
+        
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(java.net.URI.create("https://dweet.io/dweet/for/agent1?message=" + "tofriends"))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(jsonBody.toString()))
+                .build();
+
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+        .thenApply(HttpResponse::body)
+        .thenAccept(System.out::println)
+        .join();
+        
+        signal("newMessage",msg);
+    }
 }
